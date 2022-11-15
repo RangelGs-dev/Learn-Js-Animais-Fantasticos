c7bf21da6fd81d0c469e8bf7c7192951ad35ea62
RangelGs-dev <rangelgs17@gmail.com>
2022-11-15 17:57:43 -0300

    feat(dropdown/outside)

--- efeitos-no-dom/dropdown-menu/js/script.js
+++ efeitos-no-dom/dropdown-menu/js/script.js
@@ -3,10 +3,13 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
-
+import initTooltip from './modules/tooltip.js';
+import initDropownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
+initDropownMenu();
