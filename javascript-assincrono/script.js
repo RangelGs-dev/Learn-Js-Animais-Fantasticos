f3b08eeebefbb17cfaa0af52689eac02b6219159
RangelGs-dev <rangelgs17@gmail.com>
2023-03-18 22:57:32 -0300

    feat(Course accumulation 18-03-2023)

---
+++ javascript-assincrono/script.js
@@ -0,0 +1,40 @@
+/* const promessa = new Promise(function(resolve, reject) {
+    let condition = true;
+
+    if(!condition) {
+        setTimeout(() => {
+            resolve({nome: 'Rangel', idade: 26});
+        }, 5000);
+    } else {
+        reject(Error('Ocorreu um erro!'));
+    }
+});
+
+const retorno = promessa.then(function(resolucao) {
+    resolucao.profissao = 'Dev FrontEnd'
+    return resolucao
+}).then(function(resolucao) {
+    console.log(resolucao)
+}).catch(function(rejeitada) {
+    console.log(rejeitada)
+}).finally(() => {
+    console.log('Acabou')
+}) */
+// Tratamento de erro pode ser passado como segundo parametro do then()
+//console.log(retorno)
+
+const login = new Promise(function(resolve) {
+    setTimeout(() => {
+        console.log('Usuario logado')
+    }, 1000);
+})
+
+const dados = new Promise(function(resolve) {
+    setTimeout(() => {
+        console.log('Dados carregados')
+    }, 1500);
+})
+
+const loadAll = Promise.all([login, dados])
+
+console.log(loadAll)
